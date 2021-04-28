5bd22e49c241c88ab174b1a8d9e6e01e6fdaa436
brandonTsai <brandonya125@gmail.com>
2021-04-28 07:59:27 -0700

    Import Dexie.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import './App.css';
+import Dexie from 'dexie';
+
+//
+// Declare Database
+//
+const db = new Dexie("FriendDatabase");
+db.version(1).stores({ friends: "++id,name,age" });
+
+db.transaction('rw', db.friends, async() => {
+
+    // Make sure we have something in DB:
+    if ((await db.friends.where({name: 'Josephine'}).count()) === 0) {
+        const id = await db.friends.add({name: "Josephine", age: 21});
+        alert (`Addded friend with id ${id}`);
+    }
+
+    // Query:
+    const youngFriends = await db.friends.where("age").below(25).toArray();
+
+    // Show result:
+    alert ("My young friends: " + JSON.stringify(youngFriends));
+
+}).catch(e => {
+    alert(e.stack || e);
+});
 
 function App() {
+
   return (
     <div className="App">
       <h1>Let's Get Start!</h1>
